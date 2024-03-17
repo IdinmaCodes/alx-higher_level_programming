b2e5e421b7da624cf177a4fee84045f2e3afa029
IdinmaCodes <idigweamaka@gmail.com>
2024-03-16 18:10:07 -0700

    changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-print("Last digit of", number, "is", last_digit, end=" ")
+
+# Extract the last digit using the modulo operator
+last_digit = number % 10
+
+# Print the required output based on conditions
+print("Last digit of", number, "is", last_digit, end="")
+
 if last_digit > 5:
-    print(" and is greater than 5")
+   print(" and is greater than 5")
 elif last_digit == 0:
-    print(" and is 0")
-else:
-    print(" and is less than 6 and not 0")
+   print(" and is 0")
+else:  # last_digit < 6 and not 0
+   print(" and is less than 6 and not 0")
