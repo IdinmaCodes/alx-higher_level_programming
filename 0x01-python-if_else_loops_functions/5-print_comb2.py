de6f39e90aada3c795295538d6880054ebc9107d
IdinmaCodes <idigweamaka@gmail.com>
2024-03-27 10:19:02 -0700

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if num < 99:
-        print(f"{num:02,}", end=", ")
+        print(f"{num:02,}".format(num=num), end=", ")
     else:
-        print(f"{num:02}")
+        print(f"{num:02}".format(num=num))
