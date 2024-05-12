0cff80d568157cd52f16b31e3be74966481ecf62
Idinma <idigweamaka@gmail.com>
2024-05-12 12:16:18 +0100

    Hexadecimal printing

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
-for num in range(0, 98+1):
-    print("{:d} = {}".format(num, hex(num)))
+##!/usr/bin/python3
+#for num in range(0, 98+1):
+#    print("{:d} = {}".format(num, hex(num)))
+
+for i in range(0, 98+1):
+      print("{}: 0x{}".format(i, hex(i)))
