e7241f63d90d5394d196bf3e936470e1b8665fe8
Idinma <idigweamaka@gmail.com>
2024-05-28 15:42:47 +0100

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,6 @@ def uppercase(str):
 	for char in str:
 		if char >= 'a' and char <= 'z':
 			uppercase_str += chr(ord(char)- 32)
+		else:
+			uppercase_str += char
 	print("{}".format(uppercase_str))
