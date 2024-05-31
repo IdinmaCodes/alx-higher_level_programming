639575b592d16d97b1965fb9858d1a93b9ff47de
IdinmaCodes <idigweamaka@gmail.com>
2024-05-31 03:00:29 -0700

    code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import add_0
 
-def add(a, b):
-    result = add_0.add(a, b)
-    print("{}+{}={}".format(a, b, result))
-
 a = 1
 b = 2
-add(a, b)
+result = add_0.add(a, b)
+print("{}+{}={}".format(a, b, result))
+
+if __name__ == "__main__":
+    pass
