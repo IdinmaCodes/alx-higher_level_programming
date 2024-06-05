0e058b3c94792e04e865adc1154e512f1b4c4076
IdinmaCodes <142584081+IdinmaCodes@users.noreply.github.com>
2024-06-05 09:00:05 +0000

    Change

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=[[]]):#code
     for row in matrix:
         for num in row:
             print("{:d}".format(num), end=" ")
