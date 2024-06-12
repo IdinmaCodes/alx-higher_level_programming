659db37efa29350b46342410348fda209a3a9c6d
IdinmaCodes <idigweamaka@gmail.com>
2024-06-12 14:08:55 +0000

    code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2 or len(tuple_b) < 2:
+        return None
+
     first_a = tuple_a[0]
     first_b = tuple_b[0]
     sum1 = first_a + first_b
-    
+
     second_a = tuple_a[1]
     second_b = tuple_b[1]
     sum2 = second_a + second_b
-    
-    return (sum1, sum2)
+
+    return sum1, sum2
