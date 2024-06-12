e005d60d07bd2e51447390cb834e81843ac62d54
IdinmaCodes <idigweamaka@gmail.com>
2024-06-12 20:32:40 +0000

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx >= len(my_list):
         return my_list
     new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
+    for index, element in enumerate(my_list):
+        if index != idx:
+            new_list.append(element)
     return new_list
