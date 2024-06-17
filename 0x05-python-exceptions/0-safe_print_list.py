7d2a434f0e49e471816e36134037ea601731877e
IdinmaCodes <idigweamaka@gmail.com>
2024-06-17 15:06:56 +0000

    Safe list print ing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     for i in my_list:
         try:
-            print(i, end=' ')
+            print(i, end='')
             counter += 1
             if counter >= x:
                 break
