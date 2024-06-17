41edfae5140d4680a1ae04ca2020cf794c8a3bbc
IdinmaCodes <idigweamaka@gmail.com>
2024-06-17 14:55:55 +0000

    Safe list print ing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3                              
-def safe_print_list(my_list=[], x=0):           
-counter = 0                                     
-for i in my_list:                                           
-                                                
-try:                                            
-print(i, end' ')
-counter += 1
-if counter >= x:
-break                            
-except exception as e:                          
-print(f"An error occurred: {e}")                
-                                                
-return counter       
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for i in my_list:
+        try:
+            print(i, end=' ')
+            counter += 1
+            if counter >= x:
+                break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    print()
+    return counter
