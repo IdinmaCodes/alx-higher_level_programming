31b9723c8d9e59f909d3ec5d44191a457fd05a1b
IdinmaCodes <idigweamaka@gmail.com>
2024-06-17 15:18:17 +0000

    Safe list print ing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """
+    Prints the first x elements of a list and returns the number of elements printed.
+    
+    Args:
+        my_list (list): The list to print from.
+        x (int): The number of elements to print.
+    
+    Returns:
+        int: The number of elements printed.
+    """
     counter = 0
+    if x > len(my_list):
+        return counter
     for i in my_list:
         try:
             print(i, end='')
