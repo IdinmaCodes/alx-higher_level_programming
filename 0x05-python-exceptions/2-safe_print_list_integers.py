d58b7f44b6ff7fa54b6b8464844abeb7f22b21a8
IdinmaCodes <idigweamaka@gmail.com>
2024-06-18 12:32:34 +0000

    print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,5 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(i, int):
                 print("{:d}".format(i))
                 count += 1
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except Exception:
     return count
