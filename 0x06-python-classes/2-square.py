3935b9876dd96c779ed45f85b8d19a535f6840f0
Idinma <142584081+IdinmaCodes@users.noreply.github.com>
2024-06-22 15:59:54 +0000

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+
 """
-This module defines a class Square that represents a square.
+Module for creating Square objects.
+
+The size must be a non-negative integer.
+
+Attributes:
+    size (int): The size of the square.
+
+Methods:
+    __init__: Initializes the Square object with a given size.
+
+Raises:
+    TypeError: If the size is not an integer.
+    ValueError: If the size is less than 0.
 """
 
-S
+
 class Square:
     def __init__(self, size=0):
         if type(size) is not int:
@@ -11,3 +24,4 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+zez
