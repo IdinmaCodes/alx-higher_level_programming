5d6e14ee9c3bbbc75636f36dd13fe21018f6d9eb
Idinma <idigweamaka@gmail.com>
2024-06-24 11:34:39 +0100

    Printign a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 """
-A class used to represent a Square
-
-...
-
-Attributes
-----------
-size : int
-    the size of the sides of the square
-
-Methods
--------
-area():
-    Returns the area of the square.
-
-my_print():
-    Prints the square using '#' characters.
+This module defines a Square class.
+The Square class represents a square with size validation and methods
+to compute its area and print the square with '#' characters.
 """
 
 
 class Square:
+    """
+    Square class represents a square object.
+
+    Attributes:
+        __size (int): Size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance
+        with a given size (default is 0).
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square,
+        with type and value validation.
+        area(self): Computes and returns the area of the square.
+        my_print(self): Prints the square with '#' characters.
+        If size is 0, prints an empty line.
+    """
+
     def __init__(self, size=0):
         self.size = size
 
