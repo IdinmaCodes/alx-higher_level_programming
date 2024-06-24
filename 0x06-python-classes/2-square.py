61b21dc6dc6590b0cb89cbaf753fb90cbd7c16a2
Idinma <idigweamaka@gmail.com>
2024-06-24 11:41:21 +0100

    size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-
 """
-Module for creating Square objects.
-
-The size must be a non-negative integer.
-
-Attributes:
-    size (int): The size of the square.
-
-Methods:
-    __init__: Initializes the Square object with a given size.
-
-Raises:
-    TypeError: If the size is not an integer.
-    ValueError: If the size is less than 0.
+This module defines a Square class.
+The Square class represents a square with size validation.
 """
 
 
 class Square:
+    """
+    Square class represents a square object.
+
+    Attributes:
+        __size (int): Size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance
+        with a given size (default is 0).
+    """
+
     def __init__(self, size=0):
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
