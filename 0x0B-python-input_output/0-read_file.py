bfba8859d921ecda15ad9ad577d9bc42b4f5afcb
Idinma <idigweamaka@gmail.com>
2024-06-27 21:29:26 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,6 @@ text file and print its contents to stdout.
 
 
 def read_file(filename=""):
+    """Reads and prints the contents of a text file."""
     with open(filename, 'r', encoding='UTF8') as file:
         print(file.read())
