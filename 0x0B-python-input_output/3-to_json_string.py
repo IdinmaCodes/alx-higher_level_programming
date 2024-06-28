9ed541620504089ae05b57852fd131508cf70b79
Idinma <idigweamaka@gmail.com>
2024-06-28 09:35:12 +0100

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: json_representation
+Provides a function to return the JSON string of an object.
+"""
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns JSON string of an object.
+    """
+    return json.dumps(my_obj)
